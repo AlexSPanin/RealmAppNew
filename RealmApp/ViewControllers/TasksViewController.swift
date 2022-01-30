8fac0df4e2f63b7ceb35fb59c157a41f39754807
Alexander Panin <panin1970@gmail.com>
2022-01-30 18:55:19 +0300

    add realm

--- RealmApp/ViewControllers/TasksViewController.swift
+++ RealmApp/ViewControllers/TasksViewController.swift
@@ -74,9 +74,9 @@ class TasksViewController: UITableViewController {
 //        tableView.reloadData()
 //    }
 //    
-//    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-//        true
-//    }
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
     
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
